a6aeee320fe3a199ab63ad9779401144a07ff7c2
josephatj <josephatjulius24@gmail.com>
2017-11-09 15:52:36 +0300

    landing and navbar-home changed

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -12,29 +12,30 @@ export class LandingComponent implements OnInit {
 
   ngOnInit() {
     $('.landing-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav: false,
+      loop: true,
+      margin: 10,
+      nav: true,
+      navText: ["<span class='fa fa-chevron-left icon-white' style='color: gray; font-size: 30px'></span>", "<span class='fa fa-chevron-right icon-white' style='color: gray; font-size: 30px'></span>"],
       dots: true,
       dotsContainer: '#carousel-custom-dots',
-      responsiveClass:true,
-      responsive:{
-        0:{
-          items:1,
-          loop:true,
-          autoplay:true,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          loop: true,
+          autoplay: true,
           autoplaySpeed: 2000
         },
-        600:{
-          items:1,
-          loop:true,
-          autoplay:true,
+        600: {
+          items: 1,
+          loop: true,
+          autoplay: true,
           autoplaySpeed: 2000
         },
-        1000:{
-          items:1,
-          loop:true,
-          autoplay:true,
+        1000: {
+          items: 1,
+          loop: true,
+          autoplay: true,
           autoplaySpeed: 2000
         }
       }
@@ -42,6 +43,25 @@ export class LandingComponent implements OnInit {
     $('.owl-dot').click(function () {
       $('.landing-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
     });
+
+    $('.owl-prev').css('position', 'absolute');
+    $('.owl-next').css('position', 'absolute');
+    // $('.owl-prev').css('height', '45px');
+    // $('.owl-next').css('height', '45px');
+    // $('.owl-prev').css('width', '25px');
+    // $('.owl-next').css('width', '25px');
+    $('.owl-prev').css('top', '45%');
+    $('.owl-next').css('top', '45%');
+
+    // $('.owl-prev').css('borderBottomRightRadius', '40px');
+    // $('.owl-prev').css('borderTopRightRadius', '40px');
+    // $('.owl-next').css('borderBottomLeftRadius', '50px');
+    // $('.owl-next').css('borderTopLeftRadius', '50px');
+
+    $('.owl-next').css('right', '5px');
+    $('.owl-prev').css('marginLeft', '5px');
+    // $('.owl-prev').css('backgroundColor', '#F5F5F5');
+    // $('.owl-next').css('backgroundColor', '#F5F5F5');
   }
 
 }
